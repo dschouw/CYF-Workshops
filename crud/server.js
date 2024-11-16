62e479ecdfa1dab522729961d3a52e70ed624c4a
Denver Schouw <dschouw@gmail.com>
2024-11-16 11:48:04 +0200

    feat(api): first route /get - return all albums

--- crud/server.js
+++ crud/server.js
@@ -3,5 +3,15 @@ const albumsData = require("./albums.json");
 const app = express();
 
 app.listen(3000, () => {
-  console.log("Server is listening on port 3000. Ready to accept requests!");
+  console.log("Server is listening on port 3000 using express. I am Ready to accept requests!");
 });
+
+
+// This is the albums endpoint. IT will return all ALbums data available
+// Parameters : none
+// Return : all data from albums.json
+app.get("/albums", (req, res) => {
+  res.send(albumsData);
+});
+
+// Get specific album by id
